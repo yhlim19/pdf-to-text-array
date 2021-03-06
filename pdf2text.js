a5676c116393b913c4e43aa0a750cf31c1aaa379
yihan <yhlim0710@gmail.com>
2021-03-06 14:24:14 +0800

    first commit

---
+++ pdf2text.js
@@ -0,0 +1,42 @@
+var path = require('path')
+var filePath = path.join(__dirname, './sample3.pdf')
+var extract = require('pdf-text-extract')
+extract(filePath, { splitPages: false }, function (err, data) {
+  if (err) {
+    console.dir(err)
+    return
+  }
+  //console.dir(data)
+  
+//extract string from Json Formatted array
+  finalString = '';
+  for (i=0; i<data.length; i++){
+    finalString += data[i];
+  }
+  //console.log(finalString);
+
+// Replace all regex in the string
+  finalString = finalString.replace(/(\r\n|\n|\r)/gm, ""); 
+  filteredString = finalString.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+  //console.log(filteredString)
+
+// Convert String back to Array 
+  string_to_array = function(str){
+    return str.trim().split(" ");
+  };
+  suspWordArray = string_to_array(filteredString);
+  //console.log(suspWordArray);
+
+// Remove empty elements in array
+  filteredArray = suspWordArray.filter(el =>{
+    return el != '' && el != null;
+  });
+
+// Filter repeated values in array
+  uniqueArray = filteredArray.reduce(function(a,b){
+    if (a.indexOf(b)<0 ) a.push(b);
+    return a;
+  },[]);
+  console.log(uniqueArray);
+
+})
